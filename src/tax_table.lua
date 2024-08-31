c9d419c156d7fa48732c6132677aa3b943e28e77
hibbz <35602232+hibbz@users.noreply.github.com>
2024-08-31 13:50:49 -0400

    Update tax_table.lua

--- src/tax_table.lua
+++ src/tax_table.lua
@@ -17,16 +17,16 @@ end
 Utils.Print_array(Constants.State_list)
 local state = Ask_state()
 
-
 function Return_taxrate(the_array, str)
-    for i = 1,#the_array,1
-    do
+    for i = 1, #the_array, 1 do
         local x = (the_array[i])
         if ((string.lower(x[1]) == string.lower(str)) or (string.lower(x[2]) == string.lower(str))) then
             return x[2]
         end
     end
+    print(state .. " is not a state")
 end
+
 
 State_info = Return_taxrate(Constants.State_taxrate, state)
 
@@ -34,8 +34,10 @@ function Ask_info()
     print("What would you like to know about " .. State_info .. ":", "\nSales Tax - State Tree - Most Famous For")
     local selected_info = io.read();
     return selected_info
-
 end
+
+
+
 
 
 
@@ -88,9 +90,9 @@ function Compare_question(the_array, question)
 end
 
 
-
-local questions = Ask_info()
---local check = State_check(Constants.State_taxrate)
-Question_info = Return_question(Constants.Question_List, questions)
-Compare_question(Constants.State_taxrate, Question_info)
-
+if (State_info) then
+    local questions = Ask_info()
+    -- local check = State_check(Constants.State_taxrate)
+    Question_info = Return_question(Constants.Question_List, questions)
+    Compare_question(Constants.State_taxrate, Question_info)
+end
